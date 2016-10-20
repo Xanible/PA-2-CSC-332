8fa65529392f53ced1ead4033e7ef2bab3d08167
Xanible <cbp1222@jagmail.southalabama.edu>
2016-10-20 12:41:23 -0500

    new methods

--- PA-2-Source.cpp
+++ PA-2-Source.cpp
@@ -16,6 +16,11 @@ void randomizeArrays(array<array<int,9>, 9000> array_i, int i) {
 	}
 }
 
+void mergesort(array<array<int,9>, 9000> array_i, int i) {
+	// I need teh mergesort code herez
+	// Get rid of these comments when you do please
+}
+
 
 int main(int argc, char** argv) {
 	// Declare the 9 arrays
@@ -26,17 +31,24 @@ int main(int argc, char** argv) {
 		randomizeArrays(array_i, i);
 	}
 	
+	// Output file for text
+	string junk = "Mergesort_Time.txt";
+	ofstream ms_times(junk.c_str());
+	
 	// The console loop
 	char input = 0;
 	
 	cout << "Hello! This is a console application." << endl;
 	cout << "Press q to quit, press a to execute foo." << endl;	
 	while(1) {
-		cin >> input;
-		if(input == 'a') {
-			//Foo();
-		} else if(input == 'q') {
+		cin >> input;// Grab the first character
+    	if (cin.get() != '\n')  {
+        	cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the input stream
+        	cout << "Enter a number 1 - 9: \n";
+    	} else if(input == 'q') {
 			break;
+		} else if((input - 48) < 10 && (input - 48) > 0) {
+			mergesort(array_i, input - 48);
 		} else if(input != '\n') {
 			cout << "Unknown command '" << input << "'! Ignoring...\n";
 		}
