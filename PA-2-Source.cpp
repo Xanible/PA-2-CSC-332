e8f8f7ea198102933350e3637afd6fac9ad80492
spy998 <spy998lper@gmail.com>
2016-10-20 16:05:19 -0500

    Update PA-2-Source.cpp

--- PA-2-Source.cpp
+++ PA-2-Source.cpp
@@ -148,13 +148,22 @@ int main(int argc, char** argv) {
 	test(array_i);
 	
 	cout << "Hello! This is a console application." << endl;
-	cout << "Press q to quit, press a to execute foo." << endl;	
+	cout << "Press 1 to see array 1/n"
+		"Press 2 to see array 2/n"
+		"Press 3 to see the third array/n"
+		"Press 4 to see the fourth array/n"
+		"Press 5 to see the fifth array/n"
+		"Press 6 to see the sixth array/n"
+		"Press 7 to see the seventh array/n"
+		"Press 8 to see the eighth array/n"
+		"Press 9 to see the ninth array/n"
+		"Press q to quit"<< endl;	
 	while(1) {
 		cin >> input;
     	if (cin.get() != '\n')  {
         	cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Clear the input stream
         	cout << "Enter a number 1 - 9: \n";
-    	} else if(input == 'q') {
+    	} else if(input == 'q' || input == 'Q') {
 			break;
 		} else if((input - 48) < 10 && (input - 48) > 0) {
 			// Show the unsorted array
