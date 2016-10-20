9c5d49f48cc969ac598b57d7d418fab916044bec
Xanible <cbp1222@jagmail.southalabama.edu>
2016-10-20 11:12:25 -0500

    Completed function to fill array

--- PA-2-Source.cpp
+++ PA-2-Source.cpp
@@ -5,18 +5,28 @@
 
 using namespace std;
 
-void randomizeArray(int array[]) {	
+void randomizeArrays(array<array<int,9>, 9000> array_i, int i) {	
 	// Seed random number generator. 
 	srand((unsigned)time(NULL));
 
 	// A random number in the range of 1 to 100,000 is assigned.
-	for(int i=0;i < 1000;i++) {
-		array[i] = rand() % 100000 + 1;
+	int bound = (i + 1) * 1000;
+	for(int j=0;j < bound;j++) {
+		array_i[j][i] = rand() % 100000 + 1;
 	}
 }
 
 
 int main(int argc, char** argv) {
+	// Declare the 9 arrays
+	array<array<int,9>, 9000> array_i = {}; 
+	
+	// Fill the arrays based on size
+	for (int i = 0;i < 9;i++) {
+		randomizeArrays(array_i, i);
+	}
+	
+	// The console loop
 	char input = 0;
 	
 	cout << "Hello! This is a console application." << endl;
@@ -24,7 +34,7 @@ int main(int argc, char** argv) {
 	while(1) {
 		cin >> input;
 		if(input == 'a') {
-			Foo();
+			//Foo();
 		} else if(input == 'q') {
 			break;
 		} else if(input != '\n') {
